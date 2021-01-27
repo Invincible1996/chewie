7cb8ab298f23994a4b69245bbecdc80bee18b83c
zhangwenqiang <13701701451@163.com>
2021-01-27 17:49:02 +0800

    style: 更换字体图标库

--- lib/src/common_icons.dart
+++ lib/src/common_icons.dart
@@ -9,9 +9,9 @@ import 'package:flutter/material.dart';
 
 class CommonIcons {
   static const _iconFamily = 'CommonIcons';
+  static const IconData icon_arrow_back2 = const IconData(0xe657, fontFamily: _iconFamily);
   static const IconData icon_video_play = const IconData(0xe63e, fontFamily: _iconFamily);
   static const IconData icon_video_pause = const IconData(0xe63c, fontFamily: _iconFamily);
   static const IconData icon_video_full_screen = const IconData(0xe63d, fontFamily: _iconFamily);
   static const IconData icon_arrow_back = const IconData(0xe657, fontFamily: _iconFamily);
-  static const IconData icon_arrow_back2 = const IconData(0xe64e, fontFamily: _iconFamily);
 }
